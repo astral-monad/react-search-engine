9f75d54bc3df39eab6d1f2e7891335f61bb7dd68
westher0870 <80432333+westher0870@users.noreply.github.com>
2021-04-04 12:43:06 -0400

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -31,7 +31,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "a580b1e10de808f6fcf0f8a37bfac94f";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
