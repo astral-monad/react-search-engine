ee3b379dc121f56d7adbf4b9838a844de7438a22
westher0870 <80432333+westher0870@users.noreply.github.com>
2021-04-04 12:58:27 -0400

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -4,7 +4,7 @@ export default function WeatherTemperature(props) {
   const [unit, setUnit ] = useState("celsius");
   function showFahrenheit(event) {event.preventDefault();
   setUnit("Fahrenheit");
-  }
+  } }
 
   function showCelsius(event) {event.preventDefault();
   setUnit("celsius");
